f79e43af0d1bd36598714758ada35858a4673743
Lucas Pelutre <pelutrelucas@gmail.com>
2024-07-19 16:28:25 -0300

    Add files via upload

---
+++ 6Repeticao_While/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Console.WriteLine("## Instrução While ##\n");
+
+int numero;
+int contador = 1;
+
+Console.WriteLine("\nDigite um número maior que zero\t");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0)
+{
+    //loop while
+    Console.WriteLine($"\n## Tabuada do {numero} ##\n");
+    while (contador < 11)
+    {
+        Console.WriteLine($"{numero} x {contador} = {numero*contador}");
+        contador++;
+    }
+}
+else
+{
+    Console.WriteLine("\nNúmero deve ser maior que zero!");
+}
+Console.WriteLine("\nFim do Processamento!");
+Console.ReadKey();
